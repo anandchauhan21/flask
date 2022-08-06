ed2d96f57c9670852300ea39db1d68711bced828
anandchauhan21 <anandchauhan8791@gmail.com>
2022-08-06 13:46:09 +0530

    make simple url

---
+++ app.py
@@ -0,0 +1,27 @@
+# save this as app.py
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World! this is my fisrt flsk app"
+
+@app.route('/hello/')
+def hello():
+    return 'this is my first home page'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
